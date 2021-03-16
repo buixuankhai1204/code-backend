5b5fbe4dbe934442ac0ddc3f9a4531350d982777
buixuankhai1204 <khaibuixuan2@gmail.com>
2021-03-16 11:53:43 +0700

    3

--- site/queue.php
+++ site/queue.php
@@ -2,30 +2,34 @@
 require('phpmailer/phpmailer/src/PHPMailer.php');
 require('phpmailer/phpmailer/src/SMTP.php');
 require('phpmailer/phpmailer/src/Exception.php');
+
 use PHPMailer\PHPMailer\SMTP;
 
 use PHPMailer\PHPMailer\PHPMailer;
+
 try {
     $redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
-$key = "keys";
-$mail=new PHPMailer();
-do {
-    if ($redis->get($key)) {
-        $inputName=$redis->get($key)->nameEmail;
-        $inputTitle=$redis->get($key)->nameEmail;
-        $inputName=$redis->get($key)->nameEmail;
-        $output = $redis->get($key);
-        $mail->addAddress($inputName);
-        $mail->Subject = $inputTitle;
-        $mail->Body    = $inputContent;
-        $mail->send();
-        $redis->DEL($key);
-        
-    }
-    echo"ok";
-} while (true);
+    $redis->connect('127.0.0.1', 6378);
+    $mail = new PHPMailer();
+    do {
+        if ($redis->get('key')) {
+            $array=json_decode($redis->get('key'),true);
+            echo '<pre>';
+            print_r($array);
+            echo '</pre>';
+            $inputTitle = $array[0]->nameEmail;
+            $inputName = $array[0]->titleEmail;
+            $inputContent = $array[0]->contentEmail;
+            $mail->addAddress($inputName);
+            $mail->Subject = $inputTitle;
+            $mail->Body    = $inputContent;
+            $mail->send();
+            unset($array[0]);
+            array_values($array);
+            $redis->DEL('key');
+            $redis->set('key', json_encode($array));
+        }
+    } while (true);
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-
--- site/sendMail.php
+++ site/sendMail.php
@@ -4,36 +4,36 @@ require('phpmailer/phpmailer/src/PHPMailer.php');
 require('phpmailer/phpmailer/src/SMTP.php');
 require('phpmailer/phpmailer/src/Exception.php');
 use PHPMailer\PHPMailer\SMTP;
-
 use PHPMailer\PHPMailer\PHPMailer;
-        
-class sendMail extends PHPMailer {
-    
-    
+
+class sendMail extends PHPMailer
+{
     function __construct()
-    { 
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->Port=587;
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username   = 'jshviethoang18@gmail.com';
-    $mail->Password   = 'xvrlnowfjyojmhwq';
-    $mail->setFrom('khaibuixuan2@gmail.com');
+    {
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Port = 587;
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username   = 'jshviethoang18@gmail.com';
+        $mail->Password   = 'xvrlnowfjyojmhwq';
+        $mail->setFrom('khaibuixuan2@gmail.com');
     }
 }
 $redis = new Redis();
 $redis->connect('redis', 6379);
-$key = "keys";
-$inputName = $_POST['nameEmail'];
-$inputTitle = $_POST['titleEmail'];
-$inputContent = $_POST['contentEmail'];
+$key = 'key';
 
-$arr=[
-    'nameEmail'=>$inputName,
-    'titleEmail'=>$inputTitle,
-    'contentEmail'=>$inputContent,
+$input = [
+    'nameEmail' => $_POST['nameEmail'],
+    'titleEmail' => $_POST['titleEmail'],
+    'contentEmail' => $_POST['contentEmail'],
 ];
-$redis->set($key, json_encode($arr));
-
-
+$arr=[];
+if ($redis->exists('key')) {
+    $arr = json_decode($redis->get('key'));
+    
+}
+    array_push($arr,$input);
+    $redis->DEL($key);
+    $redis->set($key, json_encode($arr));
